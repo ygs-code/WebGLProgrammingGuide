c6d17ae9dfca2ba748e540eab2d2c7ff301b644e
姚观寿 <yaoguanshou@rainbowcn.com>
2024-01-14 17:58:43 +0800

    webgl 光

--- ch08/LightedTranslatedRotatedCube.js
+++ ch08/LightedTranslatedRotatedCube.js
@@ -1,7 +1,7 @@
 // LightedTranslatedRotatedCube.js (c) 2012 matsuda
 // Vertex shader program
 var VSHADER_SOURCE =
-  'attribute vec4 a_Position;\n' +
+  'attribute vec4 a_Position;\n' +  
   'attribute vec4 a_Color;\n' +
   'attribute vec4 a_Normal;\n' +
   'uniform mat4 u_MvpMatrix;\n' +
